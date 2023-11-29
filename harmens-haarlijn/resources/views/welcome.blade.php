12e0df83190a0f59003834230201ef824e4bc65f
BadassDuckieElise <68755049+BadassDuckieElise@users.noreply.github.com>
2023-11-29 11:44:05 +0100

    nuke2

--- harmens-haarlijn/resources/views/welcome.blade.php
+++ harmens-haarlijn/resources/views/welcome.blade.php
@@ -1,17 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>testest</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
-    <body>
-    </body>
+</body>
 </html>
